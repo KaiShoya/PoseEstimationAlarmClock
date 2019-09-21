45e645355d0f9f09b5a48c14e5a7e5e1ebbacb35
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-09-22 00:23:29 +0900

    Initial Commit

---
+++ PosingAlarmClock/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PosingAlarmClock
+//
+//  Created by 甲斐翔也 on 2019/09/22.
+//  Copyright © 2019 甲斐翔也. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PosingAlarmClock/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PosingAlarmClock
+//
+//  Created by 甲斐翔也 on 2019/09/22.
+//  Copyright © 2019 甲斐翔也. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
